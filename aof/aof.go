b3005abc20b563ec787d1a30e2401e4c5a732e87
luohongsheng <15200689849@163.com>
2022-12-08 13:23:56 +0800

    数据持久化完善

--- aof/aof.go
+++ aof/aof.go
@@ -5,7 +5,10 @@ import (
 	"go-redis/interface/database"
 	"go-redis/lib/logger"
 	"go-redis/lib/utils"
+	"go-redis/resp/connection"
+	"go-redis/resp/parser"
 	"go-redis/resp/reply"
+	"io"
 	"os"
 )
 
@@ -85,5 +88,38 @@ func (handler *AofHandler) handleAof() {
 
 // LoadAof
 func (handler *AofHandler) LoadAof()  {
-	
+	file, err := os.Open(handler.aofFilename)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+	defer file.Close()
+
+	ch := parser.ParseStream(file)
+	fakeConn := &connection.Connection{}
+	for p := range ch {
+		if p.Err != nil {
+			if p.Err == io.EOF {
+				break
+			}
+			logger.Error(p.Err)
+			continue
+		}
+
+		if p.Data == nil {
+			logger.Error("empty payload")
+			continue
+		}
+
+		r, ok := p.Data.(*reply.MultiBulkReply)
+		if !ok {
+			logger.Error("need multi bulk")
+			continue
+		}
+
+		rep := handler.database.Exec(fakeConn, r.Args)
+		if reply.IsErrorReply(rep) {
+			logger.Error(rep)
+		}
+	}
 }
